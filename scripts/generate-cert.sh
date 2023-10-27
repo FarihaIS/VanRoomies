829e8697e3938f35e179178fb92eea3cfac5d740
Max Cao <maxy.cao@gmail.com>
2023-10-27 13:36:13 -0700

    have better cert generation file handling

--- scripts/generate-cert.sh
+++ scripts/generate-cert.sh
@@ -10,7 +10,12 @@ IP=$1
 if [ -z "$IP" ]; then
 	IP="127.0.0.1"
 fi
+DIR=$(dirname $(readlink -f $0))
+mkdir -p $DIR/../backend/certs
+cd $DIR/../backend/certs
 
 echo "Generating certificate for $IP"
 
 curl -sS https://raw.githubusercontent.com/antelle/generate-ip-cert/master/generate-ip-cert.sh | bash -s $IP
+
+cd -
