00de4663ec8fede8d4e7f60ae81bf7792b0744b0
Fariha Shaikh <fariha@voltsafe.com>
2023-10-28 21:40:54 -0700

    Missing class import for ViewGroup in ChatFragment

--- frontend/app/src/main/java/com/chads/vanroomies/ChatFragment.java
+++ frontend/app/src/main/java/com/chads/vanroomies/ChatFragment.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
 import java.util.ArrayList;
 
 /**
