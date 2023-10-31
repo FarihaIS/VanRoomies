fe600c8591f014626fc39199c8de9972af3a4a51
Fariha Shaikh <fariha@voltsafe.com>
2023-10-30 22:33:51 -0700

    Refactored ChatChannelActivity

--- frontend/app/src/main/java/com/chads/vanroomies/ChatChannelActivity.java
+++ frontend/app/src/main/java/com/chads/vanroomies/ChatChannelActivity.java
@@ -1,14 +1,11 @@
 package com.chads.vanroomies;
 
 // Reference: https://sendbird.com/developer/tutorials/android-chat-tutorial-building-a-messaging-ui
-import static java.util.Collections.singletonMap;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,7 +15,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import io.socket.client.Ack;
@@ -43,30 +39,66 @@ public class ChatChannelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_channel);
 
+        // Retrieve UserProfile and list of ChatMessages from ChatListAdapter
         Intent chatIntent = getIntent();
         thisUserId = chatIntent.getStringExtra("thisUserId");
         chatUser = (UserProfile) chatIntent.getSerializableExtra("otherUserProfile");
         chatMessages = (ArrayList<ChatMessage>) chatIntent.getSerializableExtra("otherUserMessages");
 
-        Map<String, String> userIdMap = new HashMap<>();
-        userIdMap.put("userId", thisUserId);
-        IO.Options socketOptions = IO.Options.builder().setAuth(userIdMap).build();
+        setUpChatSocket();
+        setUpChatChannelLayout();
+        setUpChatChannelButton();
+        updateChatChannelLayout();
+    }
 
+    private void updateChatChannelLayout() {
+        chatChannelAdapter = new ChatChannelAdapter(this, chatMessages, thisUserId);
+        chatChannelRecycler.setAdapter(chatChannelAdapter);
+        chatChannelRecycler.scrollToPosition(chatMessages.size() - 1);
+    }
+
+    private void chatSocketEmitEvent(String message) {
+        JSONObject messageObj = new JSONObject();
         try {
-            chatSocket = IO.socket(Constants.baseServerURL, socketOptions);
-            chatSocket.connect();
-        } catch (URISyntaxException e) {
-            Log.d(TAG, Log.getStackTraceString(e));
+            messageObj.put("content", message);
+            messageObj.put("to", chatUser.getUserProfileId());
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
         }
+        chatSocket.emit("private message", messageObj, (Ack) args -> {
+            JSONObject response = (JSONObject) args[0];
+            try {
+                Log.d(TAG, "Emit event response code is " + response.getString("status"));
+            }
+            catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 2023);
-        calendar.set(Calendar.MONTH, Calendar.OCTOBER);
-        calendar.set(Calendar.DAY_OF_MONTH, 28);
-        calendar.set(Calendar.HOUR_OF_DAY, 13);
-        calendar.set(Calendar.MINUTE, 5);
-        calendar.set(Calendar.SECOND, 0);
+    private void setUpChatChannelButton() {
+        chatChannelButton = findViewById(R.id.chat_send_button);
+        chatChannelButton.setOnClickListener(v -> {
+            String message = chatChannelText.getText().toString().trim();
+            if (message.isEmpty()) {
+                Toast.makeText(ChatChannelActivity.this, "Please Write Something Here", Toast.LENGTH_LONG).show();
+            }
+            else {
+                ChatMessage newMessage = new ChatMessage(thisUserId, message, System.currentTimeMillis());
+                chatMessages.add(newMessage);
+                chatSocketEmitEvent(message);
+                Log.d(TAG, "Adding new message");
 
+
+                chatChannelAdapter = new ChatChannelAdapter(ChatChannelActivity.this, chatMessages, thisUserId);
+                chatChannelRecycler.setAdapter(chatChannelAdapter);
+                chatChannelRecycler.scrollToPosition(chatMessages.size() - 1);
+            }
+            chatChannelText.setText("");
+        });
+    }
+
+    private void setUpChatChannelLayout() {
         chatChannelRecycler = findViewById(R.id.recycler_chat);
         chatChannelRecycler.setLayoutManager(new LinearLayoutManager(this));
 
@@ -87,44 +119,18 @@ public class ChatChannelActivity extends AppCompatActivity {
         }
 
         chatChannelText = findViewById(R.id.edit_chat_message);
-        chatChannelButton = findViewById(R.id.chat_send_button);
+    }
 
-        chatChannelButton.setOnClickListener(v -> {
-            String message = chatChannelText.getText().toString().trim();
-            if (TextUtils.isEmpty(message)) {//if empty
-                Toast.makeText(ChatChannelActivity.this, "Please Write Something Here", Toast.LENGTH_LONG).show();
-            } else {
-                // TODO: PUT request to send message
-                Log.d(TAG, "Adding new message");
-                ChatMessage newMessage = new ChatMessage(thisUserId, message, System.currentTimeMillis());
+    private void setUpChatSocket() {
+        Map<String, String> userIdMap = new HashMap<>();
+        userIdMap.put("userId", thisUserId);
+        IO.Options socketOptions = IO.Options.builder().setAuth(userIdMap).build();
 
-                JSONObject messageObj = new JSONObject();
-                try {
-                    messageObj.put("content", message);
-                    messageObj.put("to", chatUser.getUserProfileId());
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-                chatSocket.emit("private message", messageObj, (Ack) args -> {
-                    JSONObject response = (JSONObject) args[0];
-                    try {
-                        Log.d(TAG, "Emit event response code is " + response.getString("status"));
-                    }
-                    catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-                chatMessages.add(newMessage);
-                chatChannelAdapter = new ChatChannelAdapter(ChatChannelActivity.this, chatMessages, thisUserId);
-                chatChannelRecycler.setAdapter(chatChannelAdapter);
-                chatChannelRecycler.scrollToPosition(chatMessages.size() - 1);
-            }
-            chatChannelText.setText("");
-        });
-
-        chatChannelAdapter = new ChatChannelAdapter(this, chatMessages, thisUserId);
-        chatChannelRecycler.setAdapter(chatChannelAdapter);
-        chatChannelRecycler.scrollToPosition(chatMessages.size() - 1);
+        try {
+            chatSocket = IO.socket(Constants.baseServerURL, socketOptions);
+            chatSocket.connect();
+        } catch (URISyntaxException e) {
+            Log.d(TAG, Log.getStackTraceString(e));
+        }
     }
 }
