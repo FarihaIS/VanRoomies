b3e879e646e931fdc812f44a586054d45019b4a6
Fariha Shaikh <fariha@voltsafe.com>
2023-11-01 20:41:21 -0700

    excludeUserFromFutureMatches workinggit status startChat not working

--- frontend/app/src/main/java/com/chads/vanroomies/MatchesFragment.java
+++ frontend/app/src/main/java/com/chads/vanroomies/MatchesFragment.java
@@ -18,7 +18,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.MultipartBody;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -165,14 +165,15 @@ public class MatchesFragment extends Fragment {
 
     private void excludeUserFromFutureMatches(OkHttpClient client, Activity activity, View v, String matchUserId) {
         String url = Constants.baseServerURL + Constants.userEndpoint + thisUserId + Constants.matchesByUserIdEndpoint;
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("excludedId", matchUserId)
+        Log.d(TAG, "Exclude userId " + matchUserId);
+        RequestBody requestBody = new FormBody.Builder()
+                .add("excludedId", matchUserId)
                 .build();
         Request request = new Request.Builder()
                 .url(url)
                 .put(requestBody)
                 .build();
+
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
@@ -196,10 +197,9 @@ public class MatchesFragment extends Fragment {
 
     private void startChatWithMatchedUser(OkHttpClient client, Activity activity, View v, String matchUserId) {
         String url = Constants.baseServerURL + Constants.chatsByUserIdEndpoint + thisUserId;
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("to", matchUserId)
-                .addFormDataPart("content", Constants.defaultFirstMessage)
+        RequestBody requestBody = new FormBody.Builder()
+                .add("to", matchUserId)
+                .add("content", Constants.defaultFirstMessage)
                 .build();
         Request request = new Request.Builder()
                 .url(url)
