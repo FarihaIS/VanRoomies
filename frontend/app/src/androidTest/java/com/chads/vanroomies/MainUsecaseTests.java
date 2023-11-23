39db4660759f10a59378800f2b29b0c260ef7980
Fariha Shaikh <fariha@voltsafe.com>
2023-11-22 18:35:36 -0800

    Updated MainUsecaseTests to contain tests for matches; all working now

--- frontend/app/src/androidTest/java/com/chads/vanroomies/MainUsecaseTests.java
+++ frontend/app/src/androidTest/java/com/chads/vanroomies/MainUsecaseTests.java
@@ -6,6 +6,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -16,6 +17,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
 
 import android.util.Log;
 import android.view.View;
@@ -23,6 +25,10 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.GeneralLocation;
+import androidx.test.espresso.action.GeneralSwipeAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Swipe;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -31,20 +37,28 @@ import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiSelector;
 
+import com.daprlabs.cardstack.SwipeDeck;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MainUsecaseTests {
     final static String TAG = "MainUsecaseTests";
     private UiDevice mUiDevice;
+    private ArrayList<String> matches = new ArrayList<>(Arrays.asList("John Man", "Mary Jane", "John Doe", "Kevin Zhang"));
+
     public void loginHelper() throws Exception {
         // Google Sign-In using UiDevice in conjunction with Espresso for third party popup compatibility
         ViewInteraction id = onView(
@@ -345,6 +359,203 @@ public class MainUsecaseTests {
 
         // Return to listings fragment
         pressBack();
+    }
+
+    @Test
+    public void matchesTest() throws Exception {
+        UiObject login = mUiDevice.findObject(new UiSelector().textContains("Sign In"));
+        if (login.exists()) {
+            loginHelper();
+        }
+
+        // Click on matches fragment
+        ViewInteraction bottomNavigationItemView = onView(
+                Matchers.allOf(withId(R.id.menu_matches), withContentDescription("Matches"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Check card deck is not empty
+        ViewInteraction swipeDeck = onView(
+                Matchers.allOf(withId(R.id.matches_swipe_deck),
+                        childAtPosition(
+                                childAtPosition(withId(R.id.matches_relative_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        swipeDeck.check((view, noViewFoundException) -> {
+            if (view instanceof SwipeDeck) {
+                int childCount = ((SwipeDeck) view).getChildCount();
+                assertTrue(childCount > 0);
+            }
+        });
+
+        // Swipe left on "John Man"
+        swipeDeck.perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER, GeneralLocation.CENTER_LEFT, Press.FINGER));
+
+        // For loop to swipe left through all other matches
+        for (int i = 0; i < matches.size()-1; i++) {
+            onView(Matchers.allOf(withId(R.id.matches_name), withText(matches.get(i % 4)),
+                    withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                    isDisplayed()));
+            swipeDeck.perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER, GeneralLocation.CENTER_LEFT, Press.FINGER));
+        }
+
+        // Swipe right on "John Man" this time
+        swipeDeck.perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER, GeneralLocation.CENTER_RIGHT, Press.FINGER));
+
+        // Click on chat fragment
+        bottomNavigationItemView = onView(
+                Matchers.allOf(withId(R.id.menu_chat), withContentDescription("Chat"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                3),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Check chat is with "John Man"
+        ViewInteraction chatNameTextView = onView(
+                Matchers.allOf(withId(R.id.chat_row_name), withText("John Man"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        chatNameTextView.check(matches(withText("John Man")));
+
+        // Click on chat with "John Man"
+        ViewInteraction chatRecyclerView = onView(
+                Matchers.allOf(withId(R.id.chatlistrecycle),
+                        childAtPosition(
+                                withClassName(Matchers.is("android.widget.FrameLayout")),
+                                0)));
+        chatRecyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        // Check last message is "Hello!"
+        ViewInteraction chatMessageTextView = onView(
+                Matchers.allOf(withId(R.id.chat_me_text), withText("Hello!"),
+                        withParent(Matchers.allOf(withId(R.id.chat_me_container),
+                                withParent(withId(R.id.chat_me_message)))),
+                        isDisplayed()));
+        chatMessageTextView.check(matches(withText("Hello!")));
+
+        // Click back
+        pressBack();
+
+        // Click on matches fragment
+        bottomNavigationItemView = onView(
+                Matchers.allOf(withId(R.id.menu_matches), withContentDescription("Matches"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Check card deck is not empty
+        swipeDeck = onView(
+                Matchers.allOf(withId(R.id.matches_swipe_deck),
+                        childAtPosition(
+                                childAtPosition(withId(R.id.matches_relative_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        swipeDeck.check((view, noViewFoundException) -> {
+            if (view instanceof SwipeDeck) {
+                int childCount = ((SwipeDeck) view).getChildCount();
+                assertTrue(childCount > 0);
+            }
+        });
+
+        // For loop to check that 'John Man' is not in matches
+        for (int i = 1; i < matches.size(); i++) {
+            ViewInteraction cardNameTextView = onView(Matchers.allOf(withId(R.id.matches_name), withText(matches.get(i % 4)),
+                    withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                    isDisplayed()));
+            cardNameTextView.check(matches(withText(matches.get(i))));
+            swipeDeck.perform(new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER, GeneralLocation.CENTER_LEFT, Press.FINGER));
+        }
+
+        // Click on chat fragment
+        bottomNavigationItemView = onView(
+                Matchers.allOf(withId(R.id.menu_chat), withContentDescription("Chat"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                3),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Click unmatch button
+        ViewInteraction unmatchButton = onView(
+                Matchers.allOf(withId(R.id.chat_row_unmatch), withContentDescription("Unmatch with User"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("androidx.cardview.widget.CardView")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        unmatchButton.perform(click());
+
+        // Check dialog message is made
+        ViewInteraction dialogAlert = onView(
+                Matchers.allOf(withId(android.R.id.message), withText("Are you sure you want to unmatch with John Man? You will lose your chat with them permanently."),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
+                        isDisplayed()));
+        dialogAlert.check(matches(withText("Are you sure you want to unmatch with John Man? You will lose your chat with them permanently.")));
+
+        // Click "Yes"
+        ViewInteraction yesButton = onView(
+                Matchers.allOf(withId(android.R.id.button1), withText("Yes"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(Matchers.is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        yesButton.perform(scrollTo(), click());
+
+        // Check there is no chat layout with name
+        ViewInteraction chatFragment = onView(
+                Matchers.allOf(withParent(Matchers.allOf(withId(R.id.chatlistrecycle),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)))),
+                        isDisplayed()));
+        chatFragment.check(doesNotExist());
+
+        // Click on matches fragment
+        bottomNavigationItemView = onView(
+                Matchers.allOf(withId(R.id.menu_matches), withContentDescription("Matches"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Check card deck is not empty
+        swipeDeck = onView(
+                Matchers.allOf(withId(R.id.matches_swipe_deck),
+                        childAtPosition(
+                                childAtPosition(withId(R.id.matches_relative_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        swipeDeck.check((view, noViewFoundException) -> {
+            if (view instanceof SwipeDeck) {
+                int childCount = ((SwipeDeck) view).getChildCount();
+                assertTrue(childCount > 0);
+            }
+        });
+
+        // Check that "John Man" is in list of matches again
+        onView(Matchers.allOf(withId(R.id.matches_name), withText(matches.get(0)),
+                withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
