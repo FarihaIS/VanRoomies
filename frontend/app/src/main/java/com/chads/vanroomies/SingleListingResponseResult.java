5e91d22dee02c8506fa4b7c70816c65f4e4bf1c4
“mpoon01” <mpoon01@students.cs.ubc.ca>
2023-11-28 22:45:37 -0800

    Updated SingleListingResponseResult.java. We can now enter the individual listing again!

--- frontend/app/src/main/java/com/chads/vanroomies/SingleListingResponseResult.java
+++ frontend/app/src/main/java/com/chads/vanroomies/SingleListingResponseResult.java
@@ -108,23 +108,23 @@ public class SingleListingResponseResult {
     }
 
     public static class LocationObj {
-        private int latitude;
-        private int longitude;
+        private Double latitude;
+        private Double longitude;
         private String _id;
 
-        public int getLatitude() {
+        public Double getLatitude() {
             return latitude;
         }
 
-        public void setLatitude(int latitude) {
+        public void setLatitude(Double latitude) {
             this.latitude = latitude;
         }
 
-        public int getLongitude() {
+        public Double getLongitude() {
             return longitude;
         }
 
-        public void setLongitude(int longitude) {
+        public void setLongitude(Double longitude) {
             this.longitude = longitude;
         }
 
